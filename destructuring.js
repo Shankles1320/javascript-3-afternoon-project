c8388ee66d30051cfee588a727a79e273b0933da
Carlton Kenneth Shank III <shankles@Carltons-MacBook-Pro.local>
2019-08-02 16:13:01 -0700

    desctructuring day 5

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,8 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+let { color, make, model, year} = carDetails
+console.log(carDetails);
 
 
 
@@ -34,7 +35,7 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+   var {firstName, lastName, title} = obj
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,7 +54,18 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+
+var obj = {
+  utah: 5,
+  california: 6,
+  texas: 7,
+  arizona: 8
+ }
+ function totalPopulation(obj) {
+  const {utah, california, texas, arizona} = obj;
+  var sum = ( utah + california + texas + arizona);
+ return sum;
+ }
 
 
 
@@ -67,7 +79,15 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+
+function ingredients(obj){
+  let {carb, fat, protein} = obj;
+  var arr = [];
+  arr.push(carb, fat, protein);
+
+  return arr;
+}
+
 
 
 
@@ -85,7 +105,18 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+function largeNumbers({first, second, third}){
+    if (first <= second && first <= third){
+      return first
+    }
+    if (second <= first && second <= third){
+      return second
+    }
+    if(third <= first && third <= second){
+      return third
+    }
+
+}
 
 
 
@@ -97,6 +128,17 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
+function numberGroups({a, b, c}){
+  if ( a.length >= b.length && a.length >= c.length){
+    return a;
+  }
+  if (b.length >= a.length && b.length >= c.length){
+    return b;
+  }
+  if (c.length >= a.length && c.length >= b.length){
+    return c;
+  }
+  
+}
 
 
